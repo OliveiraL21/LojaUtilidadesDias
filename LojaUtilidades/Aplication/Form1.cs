b71784f123a8ef068a5a86ef26283f5d5994c49d
Lucas Oliveira <lucasoliveira508@gmail.com>
2021-09-30 20:14:50 -0300

    criando novo projeto de aplicação NET framework

---
+++ LojaUtilidades/Aplication/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aplication
+{
+    public partial class Form_Principal : Form
+    {
+        public Form_Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Produto_MouseHover(object sender, EventArgs e)
+        {
+            btn_Produto.BackColor = Color.DarkMagenta;
+        }
+
+        private void btn_Produto_MouseLeave(object sender, EventArgs e)
+        {
+            btn_Produto.BackColor = Color.FromArgb(41, 0, 39);
+        }
+
+        private void btn_Vendas_MouseHover(object sender, EventArgs e)
+        {
+            btn_Vendas.BackColor = Color.DarkMagenta;
+        }
+
+        private void btn_Vendas_MouseLeave(object sender, EventArgs e)
+        {
+            btn_Vendas.BackColor = Color.FromArgb(41, 0, 39);
+        }
+
+
+        private void btn_Estoque_MouseHover(object sender, EventArgs e)
+        {
+            btn_Estoque.BackColor = Color.DarkMagenta;
+        }
+
+        private void btn_Estoque_MouseLeave(object sender, EventArgs e)
+        {
+            btn_Estoque.BackColor = Color.FromArgb(41, 0, 39);
+        }
+
+
+        private void btn_Sair_MouseHover(object sender, EventArgs e)
+        {
+            btn_Sair.BackColor = Color.DarkMagenta;
+        }
+
+        private void btn_Sair_MouseLeave(object sender, EventArgs e)
+        {
+            btn_Sair.BackColor = Color.FromArgb(41, 0, 39);
+        }
+
+        private void btn_Sair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form_Principal_Load(object sender, EventArgs e)
+        {
+            txt_data.Text = DateTime.Today.ToString();
+            txt_Hora.Text = DateTime.Now.TimeOfDay.ToString();
+        }
+    }
+}
