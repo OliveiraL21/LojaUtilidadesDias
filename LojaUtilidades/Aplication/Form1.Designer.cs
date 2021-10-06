f4a3c4b5bbfe30f49cd1450b2718baf5f8ebfa80
Lucas Oliveira <lucasoliveira508@gmail.com>
2021-10-06 18:03:10 -0300

    criando o primeiro formulario

---
+++ LojaUtilidades/Aplication/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace Aplication
+{
+    partial class Form_Principal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Principal));
+            this.panel_Menu = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_Sair = new System.Windows.Forms.Button();
+            this.btn_Estoque = new System.Windows.Forms.Button();
+            this.btn_Vendas = new System.Windows.Forms.Button();
+            this.btn_Produto = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_data = new System.Windows.Forms.MaskedTextBox();
+            this.txt_hora = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel_Menu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel_Menu
+            // 
+            this.panel_Menu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(0)))), ((int)(((byte)(39)))));
+            this.panel_Menu.Controls.Add(this.pictureBox1);
+            this.panel_Menu.Controls.Add(this.btn_Sair);
+            this.panel_Menu.Controls.Add(this.btn_Estoque);
+            this.panel_Menu.Controls.Add(this.btn_Vendas);
+            this.panel_Menu.Controls.Add(this.btn_Produto);
+            this.panel_Menu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel_Menu.Location = new System.Drawing.Point(0, 0);
+            this.panel_Menu.Name = "panel_Menu";
+            this.panel_Menu.Size = new System.Drawing.Size(200, 720);
+            this.panel_Menu.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(197, 175);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_Sair
+            // 
+            this.btn_Sair.FlatAppearance.BorderSize = 0;
+            this.btn_Sair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Sair.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_Sair.ForeColor = System.Drawing.Color.White;
+            this.btn_Sair.Image = ((System.Drawing.Image)(resources.GetObject("btn_Sair.Image")));
+            this.btn_Sair.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Sair.Location = new System.Drawing.Point(3, 671);
+            this.btn_Sair.Name = "btn_Sair";
+            this.btn_Sair.Size = new System.Drawing.Size(197, 49);
+            this.btn_Sair.TabIndex = 4;
+            this.btn_Sair.Text = "Sair";
+            this.btn_Sair.UseVisualStyleBackColor = true;
+            this.btn_Sair.Click += new System.EventHandler(this.btn_Sair_Click);
+            this.btn_Sair.MouseLeave += new System.EventHandler(this.btn_Sair_MouseLeave);
+            this.btn_Sair.MouseHover += new System.EventHandler(this.btn_Sair_MouseHover);
+            // 
+            // btn_Estoque
+            // 
+            this.btn_Estoque.FlatAppearance.BorderSize = 0;
+            this.btn_Estoque.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Estoque.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_Estoque.ForeColor = System.Drawing.Color.White;
+            this.btn_Estoque.Image = ((System.Drawing.Image)(resources.GetObject("btn_Estoque.Image")));
+            this.btn_Estoque.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Estoque.Location = new System.Drawing.Point(0, 385);
+            this.btn_Estoque.Name = "btn_Estoque";
+            this.btn_Estoque.Size = new System.Drawing.Size(197, 49);
+            this.btn_Estoque.TabIndex = 3;
+            this.btn_Estoque.Text = "Estoque";
+            this.btn_Estoque.UseVisualStyleBackColor = true;
+            this.btn_Estoque.MouseLeave += new System.EventHandler(this.btn_Estoque_MouseLeave);
+            this.btn_Estoque.MouseHover += new System.EventHandler(this.btn_Estoque_MouseHover);
+            // 
+            // btn_Vendas
+            // 
+            this.btn_Vendas.FlatAppearance.BorderSize = 0;
+            this.btn_Vendas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Vendas.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_Vendas.ForeColor = System.Drawing.Color.White;
+            this.btn_Vendas.Image = ((System.Drawing.Image)(resources.GetObject("btn_Vendas.Image")));
+            this.btn_Vendas.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Vendas.Location = new System.Drawing.Point(3, 295);
+            this.btn_Vendas.Name = "btn_Vendas";
+            this.btn_Vendas.Size = new System.Drawing.Size(197, 49);
+            this.btn_Vendas.TabIndex = 2;
+            this.btn_Vendas.Text = "Vendas";
+            this.btn_Vendas.UseVisualStyleBackColor = true;
+            this.btn_Vendas.MouseLeave += new System.EventHandler(this.btn_Vendas_MouseLeave);
+            this.btn_Vendas.MouseHover += new System.EventHandler(this.btn_Vendas_MouseHover);
+            // 
+            // btn_Produto
+            // 
+            this.btn_Produto.FlatAppearance.BorderSize = 0;
+            this.btn_Produto.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Produto.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_Produto.ForeColor = System.Drawing.Color.White;
+            this.btn_Produto.Image = ((System.Drawing.Image)(resources.GetObject("btn_Produto.Image")));
+            this.btn_Produto.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Produto.Location = new System.Drawing.Point(0, 198);
+            this.btn_Produto.Name = "btn_Produto";
+            this.btn_Produto.Size = new System.Drawing.Size(197, 49);
+            this.btn_Produto.TabIndex = 1;
+            this.btn_Produto.Text = "Produtos";
+            this.btn_Produto.UseVisualStyleBackColor = true;
+            this.btn_Produto.MouseLeave += new System.EventHandler(this.btn_Produto_MouseLeave);
+            this.btn_Produto.MouseHover += new System.EventHandler(this.btn_Produto_MouseHover);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(309, 218);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Data";
+            // 
+            // txt_data
+            // 
+            this.txt_data.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt_data.Location = new System.Drawing.Point(368, 218);
+            this.txt_data.Mask = "00/00/0000";
+            this.txt_data.Name = "txt_data";
+            this.txt_data.ReadOnly = true;
+            this.txt_data.Size = new System.Drawing.Size(217, 33);
+            this.txt_data.TabIndex = 2;
+            this.txt_data.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_data.ValidatingType = typeof(System.DateTime);
+            // 
+            // txt_hora
+            // 
+            this.txt_hora.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt_hora.Location = new System.Drawing.Point(368, 280);
+            this.txt_hora.Mask = "00:00";
+            this.txt_hora.Name = "txt_hora";
+            this.txt_hora.ReadOnly = true;
+            this.txt_hora.Size = new System.Drawing.Size(217, 33);
+            this.txt_hora.TabIndex = 4;
+            this.txt_hora.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_hora.ValidatingType = typeof(System.DateTime);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(309, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Hora";
+            // 
+            // Form_Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1024, 720);
+            this.Controls.Add(this.txt_hora);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_data);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel_Menu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form_Principal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form_Principal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel_Menu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_Menu;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_Sair;
+        private System.Windows.Forms.Button btn_Estoque;
+        private System.Windows.Forms.Button btn_Vendas;
+        private System.Windows.Forms.Button btn_Produto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox txt_data;
+        private System.Windows.Forms.MaskedTextBox txt_hora;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
