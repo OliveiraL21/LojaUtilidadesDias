95f42a24961535936daf89ebff5b967dbd7aa9d3
Lucas Oliveira <lucasoliveira508@gmail.com>
2022-02-01 22:30:53 -0300

    Alterando nome do Projeto de front-end

--- LojaUtilidades/Aplication/Properties/Resources.Designer.cs
+++ LojaUtilidades/Aplication/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Aplication.Properties {
+namespace Utilidades_e_Cosmeticos_Dias.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Aplication.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Aplication.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Utilidades_e_Cosmeticos_Dias.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
