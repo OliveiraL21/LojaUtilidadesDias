47b2161a8d603cac8654206687a15083e8887d7d
lucas oliveira <lucasoliveira508@gmail.com>
2022-02-04 18:34:38 -0300

    refatorando encapsulamento das propriedades da classe Print

--- LojaUtilidades/Aplication/Impressão/Print.cs
+++ LojaUtilidades/Aplication/Impressão/Print.cs
@@ -11,16 +11,28 @@ namespace Aplication.Impressão
 {
     public class Print : PrintDocument
     {
-        public Font letraTitulo = new Font(FontFamily.GenericSansSerif,14,FontStyle.Bold);
-        public Font letraMenu = new Font(FontFamily.GenericSansSerif, 10, FontStyle.Regular);
-        public Font letraProdutos = new Font(FontFamily.GenericSansSerif, 10, FontStyle.Regular);
-        public Font letraTotal = new Font(FontFamily.GenericSansSerif, 10, FontStyle.Bold);
-        public Brush pincelPreto = new SolidBrush(Color.Black);
-        public int contador = 100;
-        public int contador2 = 100;
-        public int x = 100;
-        public int y = 50;
-        public void ImprimirVenda(List<ProdutoEntity> produtos,double total, object sender, PrintPageEventArgs e)
+        private readonly Font letraTitulo;
+        private readonly Font letraMenu;
+        private readonly Font letraProdutos;
+        private readonly Font letraTotal;
+        private readonly Brush pincelPreto;
+        private int contador;
+        private int contador2;
+        private int x;
+        private int y;
+        public Print()
+        {
+            letraTitulo = new Font(FontFamily.GenericSansSerif, 14, FontStyle.Bold);
+            letraMenu = new Font(FontFamily.GenericSansSerif, 10, FontStyle.Regular);
+            letraProdutos = new Font(FontFamily.GenericSansSerif, 10, FontStyle.Regular);
+            letraTotal = new Font(FontFamily.GenericSansSerif, 10, FontStyle.Bold);
+            pincelPreto = new SolidBrush(Color.Black);
+            contador = 100;
+            contador2 = 100;
+            x = 100;
+            y = 50;
+        }
+        public void ImprimirVenda(List<ProdutoEntity> produtos, double total, object sender, PrintPageEventArgs e)
         {
             string empresa = "Cosméticos e Utilidades Dias";
             string cnpj = "Cnpj: 29.936.014/0001-01";
@@ -44,11 +56,11 @@ namespace Aplication.Impressão
             graphics.DrawString("Código \t\t", letraProdutos, pincelPreto, 215, y + contador2);
             graphics.DrawString("Produto\t", letraProdutos, pincelPreto, 315, y + contador2);
             graphics.DrawString("Quantidade\t", letraProdutos, pincelPreto, 420, y + contador2);
-            graphics.DrawString("Valor",letraProdutos, pincelPreto, 540, y + contador2);
+            graphics.DrawString("Valor", letraProdutos, pincelPreto, 540, y + contador2);
             contador2 += 25;
             graphics.DrawLine(new Pen(pincelPreto), 200, contador2 + 50, 630, contador2 + 50);
             contador2 += 25;
-            foreach(var produto in produtos)
+            foreach (var produto in produtos)
             {
 
                 graphics.DrawString(produto.Id + "\t\t", letraProdutos, pincelPreto, 220, y + contador2);
